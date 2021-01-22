b91140b267c10c260202c53f0feb1d3865bc9d7e
jylee2 <leej1025y@live.com>
2021-01-22 09:53:57 +0800

    Updated img src

--- script.jsx
+++ script.jsx
@@ -38,7 +38,7 @@ const App = () => {
                     <div className="col-md-8">
                         <div className="card-deck classCardDeck classProjectContainer">
                             <a className="card text-center classCardShadow classCardSize" href="https://jylee2.github.io/login-page/" target="_blank">
-                                <img className="card-img-top classCardImgHover" src="https://scontent.fkul3-2.fna.fbcdn.net/v/t1.0-9/134413511_10158235656415892_517781070568791571_n.jpg?_nc_cat=105&ccb=2&_nc_sid=0debeb&_nc_ohc=KxHvYBDm_oAAX-7afZa&_nc_ht=scontent.fkul3-2.fna&oh=9024e883588777f5a257ef62d8b08375&oe=601A2146" alt="login-page"/>
+                                <img className="card-img-top classCardImgHover" src="images/agmo-studio-01-square.png" alt="login-page"/>
                                 <div className="classCardImgHoverDiv">
                                     <div className="classCardImgHoverText">
                                         &#60; Bootstrap /&#62;
@@ -58,50 +58,8 @@ const App = () => {
                     <div className="col-xl-2"></div>
                     <div className="col-xl-8">
                         <div className="card-deck classCardDeck">
-                            <a className="card text-center classCardShadow" href="https://jylee2.github.io/javascript-calculator/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-3.fna.fbcdn.net/v/t1.0-9/131432257_10158205611795892_2889220676811119589_n.jpg?_nc_cat=106&ccb=2&_nc_sid=0debeb&_nc_ohc=rZXH0nlmDd4AX-oTzci&_nc_oc=AQnloTvw7glSRE-jigeG-_krt41vhPZL4nS_ZLweKRfAI-91MSW3gDJCW9DgbIt65Ww&_nc_ht=scontent.fkul4-3.fna&oh=15ad87b63385ba305deff43b5797bf86&oe=6007DA8E" alt="calculator"/>
-                                <div className="classCardImgHoverDiv">
-                                    <div className="classCardImgHoverText">
-                                        &#60; Bootstrap /&#62;
-                                        <br/>
-                                        &#60; React.js /&#62;
-                                    </div>
-                                </div>
-                                <div className="card-body classDivCenter classCardText">
-                                    <p className="card-title">JavaScript Calculator</p>
-                                </div>
-                            </a>
-                            <a className="card text-center classCardShadow" href="https://jylee2.github.io/25-5-clock/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-2.fna.fbcdn.net/v/t1.0-9/131761165_10158205611805892_8675711744864238880_n.jpg?_nc_cat=105&ccb=2&_nc_sid=0debeb&_nc_ohc=wXd3BpxMpo8AX8b5v8u&_nc_ht=scontent.fkul4-2.fna&oh=ecceb9bc9c78fb21f2e9596d4057e4f8&oe=6006671A" alt="pomodoro-clock"/>
-                                <div className="classCardImgHoverDiv">
-                                    <div className="classCardImgHoverText">
-                                        &#60; Bootstrap /&#62;
-                                        <br/>
-                                        &#60; React.js /&#62;
-                                    </div>
-                                </div>
-                                <div className="card-body classDivCenter classCardText">
-                                    <p className="card-title">Pomodoro Clock</p>
-                                </div>
-                            </a>
-                            <a className="card text-center classCardShadow" href="https://jylee2.github.io/choropleth-map/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-3.fna.fbcdn.net/v/t1.0-9/131672010_10158205611895892_5220015581143874202_n.jpg?_nc_cat=106&ccb=2&_nc_sid=0debeb&_nc_ohc=40IuiaSPiYgAX_JGC3R&_nc_ht=scontent.fkul4-3.fna&oh=3a208c9705e1c40f50831f95b25d4bc9&oe=600724B6" alt="choropleth-map"/>
-                                <div className="classCardImgHoverDiv">
-                                    <div className="classCardImgHoverText">
-                                        &#60; Bootstrap /&#62;
-                                        <br/>
-                                        &#60; D3.js /&#62;
-                                    </div>
-                                </div>
-                                <div className="card-body classDivCenter classCardText">
-                                    <p className="card-title">Choropleth Map</p>
-                                </div>
-                            </a>
-                        </div>
-                        <br/>
-                        <div className="card-deck classCardDeck">
                             <a className="card text-center classCardShadow" href="https://whac-a-mole.jylee3.repl.co/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul2-3.fna.fbcdn.net/v/t1.0-9/137517441_10158259638550892_3708374777962844105_n.jpg?_nc_cat=107&ccb=2&_nc_sid=0debeb&_nc_ohc=onK006Tm7SgAX8EhKLy&_nc_ht=scontent.fkul2-3.fna&oh=f8be6ec52f6856e294453bc57bd96f7d&oe=60274B6D" alt="whac-a-mole"/>
+                                <img className="card-img-top" src="images/whac-a-mole-01-square.png" alt="whac-a-mole"/>
                                 <div className="classCardImgHoverDiv">
                                     <div className="classCardImgHoverText">
                                         &#60; Bootstrap /&#62;
@@ -120,7 +78,7 @@ const App = () => {
                                 </div>
                             </a>
                             <a className="card text-center classCardShadow" href="https://exercise-tracker-project.jylee3.repl.co/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-2.fna.fbcdn.net/v/t1.0-9/133509988_10158224166260892_4427338236374963706_n.jpg?_nc_cat=104&ccb=2&_nc_sid=0debeb&_nc_ohc=JuaIUCFjE4UAX9i1uMQ&_nc_ht=scontent.fkul4-2.fna&oh=096be123b1c9a4cc3bbb935fa45e24ed&oe=6012F25F" alt="exercise-tracker"/>
+                                <img className="card-img-top" src="images/exercise-tracker-01-square.png" alt="exercise-tracker"/>
                                 <div className="classCardImgHoverDiv">
                                     <div className="classCardImgHoverText">
                                         &#60; Bootstrap /&#62;
@@ -137,7 +95,7 @@ const App = () => {
                                 </div>
                             </a>
                             <a className="card text-center classCardShadow" href="https://file-metadata.jylee3.repl.co/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-3.fna.fbcdn.net/v/t1.0-9/133289518_10158224166275892_3950579675764366296_o.jpg?_nc_cat=100&ccb=2&_nc_sid=0debeb&_nc_ohc=ZhYNQYCYYFgAX899OIA&_nc_ht=scontent.fkul4-3.fna&oh=0b57cd8aeab42ee912a241b03eec3b70&oe=60112C83" alt="file-metadata-microservice"/>
+                                <img className="card-img-top" src="images/file-metadata-01-square.png" alt="file-metadata-microservice"/>
                                 <div className="classCardImgHoverDiv">
                                     <div className="classCardImgHoverText">
                                         &#60; Bootstrap /&#62;
@@ -156,8 +114,50 @@ const App = () => {
                         </div>
                         <br/>
                         <div className="card-deck classCardDeck">
+                            <a className="card text-center classCardShadow" href="https://jylee2.github.io/javascript-calculator/" target="_blank">
+                                <img className="card-img-top" src="images/js-calculator-01-square.png" alt="calculator"/>
+                                <div className="classCardImgHoverDiv">
+                                    <div className="classCardImgHoverText">
+                                        &#60; Bootstrap /&#62;
+                                        <br/>
+                                        &#60; React.js /&#62;
+                                    </div>
+                                </div>
+                                <div className="card-body classDivCenter classCardText">
+                                    <p className="card-title">JavaScript Calculator</p>
+                                </div>
+                            </a>
+                            <a className="card text-center classCardShadow" href="https://jylee2.github.io/25-5-clock/" target="_blank">
+                                <img className="card-img-top" src="images/pomodoro-clock-square.png" alt="pomodoro-clock"/>
+                                <div className="classCardImgHoverDiv">
+                                    <div className="classCardImgHoverText">
+                                        &#60; Bootstrap /&#62;
+                                        <br/>
+                                        &#60; React.js /&#62;
+                                    </div>
+                                </div>
+                                <div className="card-body classDivCenter classCardText">
+                                    <p className="card-title">Pomodoro Clock</p>
+                                </div>
+                            </a>
+                            <a className="card text-center classCardShadow" href="https://jylee2.github.io/choropleth-map/" target="_blank">
+                                <img className="card-img-top" src="images/Choropleth-Map-01-square.png" alt="choropleth-map"/>
+                                <div className="classCardImgHoverDiv">
+                                    <div className="classCardImgHoverText">
+                                        &#60; Bootstrap /&#62;
+                                        <br/>
+                                        &#60; D3.js /&#62;
+                                    </div>
+                                </div>
+                                <div className="card-body classDivCenter classCardText">
+                                    <p className="card-title">Choropleth Map</p>
+                                </div>
+                            </a>
+                        </div>
+                        <br/>
+                        <div className="card-deck classCardDeck">
                             <a className="card text-center classCardShadow" href="https://jylee2.github.io/fcc-random-quote-machine/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-4.fna.fbcdn.net/v/t1.0-9/132308650_10158205611775892_1525069901662786222_n.jpg?_nc_cat=102&ccb=2&_nc_sid=0debeb&_nc_ohc=2-msl-3H7KMAX-pSa-o&_nc_ht=scontent.fkul4-4.fna&oh=f44bae0a84e003ddf5cdab25e654900f&oe=6005C882" alt="random-quote-generator"/>
+                                <img className="card-img-top" src="images/random-quote-machine-01-square.png" alt="random-quote-generator"/>
                                 <div className="classCardImgHoverDiv">
                                     <div className="classCardImgHoverText">
                                         &#60; Bootstrap /&#62;
@@ -170,7 +170,7 @@ const App = () => {
                                 </div>
                             </a>
                             <a className="card text-center classCardShadow" href="https://jylee2.github.io/drum-machine/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-3.fna.fbcdn.net/v/t1.0-9/132325285_10158212320775892_1353193816898006335_n.jpg?_nc_cat=106&ccb=2&_nc_sid=0debeb&_nc_ohc=AioN_G36SSIAX9DL8LG&_nc_ht=scontent.fkul4-3.fna&oh=1fa9737009e319879be35e3ae4dac5a7&oe=600C866F" alt="drum-machine"/>
+                                <img className="card-img-top" src="images/drum-machine-01-square.png" alt="drum-machine"/>
                                 <div className="classCardImgHoverDiv">
                                     <div className="classCardImgHoverText">
                                         &#60; Bootstrap /&#62;
@@ -183,7 +183,7 @@ const App = () => {
                                 </div>
                             </a>
                             <a className="card text-center classCardShadow" href="https://jylee2.github.io/scatterplot-graph/" target="_blank">
-                                <img className="card-img-top" src="https://scontent.fkul4-2.fna.fbcdn.net/v/t1.0-9/131578947_10158205671750892_2203151998922210138_o.jpg?_nc_cat=108&ccb=2&_nc_sid=0debeb&_nc_ohc=zBJWoOygtTcAX-OXZAM&_nc_ht=scontent.fkul4-2.fna&oh=b94c91d0026624028e775e37b3e81893&oe=60076660" alt="scatterplot"/>
+                                <img className="card-img-top" src="images/scatterplot-01-square.png" alt="scatterplot"/>
                                 <div className="classCardImgHoverDiv">
                                     <div className="classCardImgHoverText">
                                         &#60; Bootstrap /&#62;
